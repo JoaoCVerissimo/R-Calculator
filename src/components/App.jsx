eb6e6d643bff0d17ccf2c70ae79f51fb3bcc0c5d
João Veríssimo <j.verissimo@runtime-revolution.com>
2021-12-07 16:00:32 +0000

    Removed comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import './App.css';
 import Calculator from "./calculator/Calculator"
-// import Display from "./calculator/Display";
-// import Panel from "./calculator/Panel";
 
 
 function App() {
     return (
         <div id="root">
-            {/* <Display />
-            <Panel /> */}
             <Calculator />
         </div>
     )
