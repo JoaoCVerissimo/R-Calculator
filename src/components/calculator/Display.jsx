dad7456a8d355ef1e708679ba181bbb2d985e0ef
João Veríssimo <57098650+JoaoCVerissimo@users.noreply.github.com>
2021-12-07 17:48:53 +0000

    Update src/components/calculator/Display.jsx
    Co-authored-by: Márcia Martins <53227495+marciammart@users.noreply.github.com>

--- src/components/calculator/Display.jsx
+++ src/components/calculator/Display.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-function Display(props) {
-    const { num } = props;
-
-    return (
-        <div className="display">
-            <div>{num}</div>
-        </div>
-    )
-}
+const Display = ({ num }) => (
+    <div className="display">
+        <div>{num}</div>
+    </div>
+ )
 
 export default Display;
