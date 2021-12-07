76516344924fed56292da914786e03409878b57e
João Veríssimo <j.verissimo@runtime-revolution.com>
2021-12-07 12:41:54 +0000

    all operators working with 2 numbers

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -3,13 +3,37 @@ import React, { useState } from "react";
 
 function Calculator() {
     const [number, setNumber] = useState(0);
+    const [firstNumber, setFirstNumber] = useState(0);
+    const [operator, setOperator] = useState();
 
     function inputNumber(e) {
         parseInt(number) === 0 ? setNumber(e.target.value) : setNumber(number + e.target.value);
     }
 
-    function clear() {
-        setNumber(0)
+    function handleOperator(e) {
+        let op = e.target.value;
+        setOperator(op);
+        setFirstNumber(number);
+        setNumber(0);
+    }
+
+    function getResult() {
+        switch (operator) {
+            case "÷":
+                setNumber(parseFloat(firstNumber) / parseFloat(number));
+                break;
+            case "x":
+                setNumber(parseFloat(firstNumber) * parseFloat(number));
+                break;
+            case "+":
+                setNumber(parseFloat(firstNumber) + parseFloat(number));
+                break;
+            case "-":
+                setNumber(parseFloat(firstNumber) - parseFloat(number));
+                break;
+            default:
+                break;
+        }
     }
 
     return (
@@ -19,33 +43,33 @@ function Calculator() {
             </div>
             <div class="button-panel">
                 <div>
-                    <div class="button"><button onClick={clear}>AC</button></div>
-                    <div class="button"><button>+/-</button></div>
-                    <div class="button"><button>%</button></div>
-                    <div class="button orange"><button>÷</button></div>
+                    <div class="button"><button onClick={() => setNumber(0)}>AC</button></div>
+                    <div class="button"><button onClick={() => setNumber(number * -1)}>+/-</button></div>
+                    <div class="button"><button onClick={() => setNumber(number / 100)}>%</button></div>
+                    <div class="button orange"><button onClick={handleOperator} value={"÷"}>÷</button></div>
                 </div>
                 <div>
                     <div class="button"><button onClick={inputNumber} value={7}>7</button></div>
                     <div class="button"><button onClick={inputNumber} value={8}>8</button></div>
                     <div class="button"><button onClick={inputNumber} value={9}>9</button></div>
-                    <div class="button orange"><button>x</button></div>
+                    <div class="button orange"><button onClick={handleOperator} value={"x"}>x</button></div>
                 </div>
                 <div>
                     <div class="button"><button onClick={inputNumber} value={4}>4</button></div>
                     <div class="button"><button onClick={inputNumber} value={5}>5</button></div>
                     <div class="button"><button onClick={inputNumber} value={6}>6</button></div>
-                    <div class="button orange"><button>-</button></div>
+                    <div class="button orange"><button onClick={handleOperator} value={"-"}>-</button></div>
                 </div>
                 <div>
                     <div class="button"><button onClick={inputNumber} value={1}>1</button></div>
                     <div class="button"><button onClick={inputNumber} value={2}>2</button></div>
                     <div class="button"><button onClick={inputNumber} value={3}>3</button></div>
-                    <div class="button orange"><button>+</button></div>
+                    <div class="button orange"><button onClick={handleOperator} value={"+"}>+</button></div>
                 </div>
                 <div>
                     <div class="button  wide"><button onClick={inputNumber} value={0}>0</button></div>
                     <div class="button"><button onClick={inputNumber} value={"."}>.</button></div>
-                    <div class="button orange"><button>=</button></div>
+                    <div class="button orange"><button onClick={getResult}>=</button></div>
                 </div>
             </div>
         </div>
